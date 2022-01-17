d642b32a5dcb21eba0c71366d919fa0e7b0ede8a
Honza Sládek <me@honzasladek.com>
2022-01-17 14:25:16 +0100

    Remove reactRefresh plugin as it doesnt work

--- build/createViteConfig.js
+++ build/createViteConfig.js
@@ -43,7 +43,6 @@ export function createViteConfig(packageName) {
 				jsxFragment: '_jsxFragment',
 				jsxInject: `import { createElement as _jsx, Fragment as _jsxFragment } from 'react'`,
 			},
-			plugins: [reactRefresh()],
 			resolve: {
 				alias: [
 					...packageList.map(packageName => ({
