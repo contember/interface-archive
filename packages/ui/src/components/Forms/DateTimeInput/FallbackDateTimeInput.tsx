1ab9ddba84870fce3b2fb8e1113709f33b8f228e
Martin Adamko <attitude@users.noreply.github.com>
2022-02-02 04:04:40 +0100

    Add error validation of `datetime-locale` fallback input
    - Wrap component to mimmick singular input & update control styles

--- packages/ui/src/components/Forms/DateTimeInput/FallbackDateTimeInput.tsx
+++ packages/ui/src/components/Forms/DateTimeInput/FallbackDateTimeInput.tsx
@@ -1,10 +1,31 @@
-import { ChangeEvent, forwardRef, memo, Ref, useCallback, useEffect, useMemo, useRef } from 'react'
+import classNames from 'classnames'
+import { ChangeEvent, forwardRef, memo, Ref, useCallback, useEffect, useRef, useState } from 'react'
+import { useNativeInput, VisuallyDependententControlProps } from '..'
+import { useComponentClassName } from '../../../auxiliary'
+import { toViewClass } from '../../../utils'
+import { Divider } from '../../Divider'
 import { Stack } from '../../Stack'
+import { useInputClassName } from '../useInputClassName'
 import { assertDatetimeString, splitDatetime } from './Serializer'
 import { DateTimeInputProps } from './Types'
 
+function joinDatetime(date?: string | null, time?: string | null) {
+	return `${date}T${time}`
+}
+
 export const FallbackDateTimeInput = memo(
-	forwardRef(({ size, distinction, max, min, onChange, validationState, withTopToolbar, value, ...rest }: DateTimeInputProps, ref: Ref<HTMLInputElement>) => {
+	forwardRef(({
+		className,
+		max,
+		min,
+		onChange,
+		onValidationStateChange,
+		value: _value,
+		withTopToolbar,
+		...rest
+	}: DateTimeInputProps, ref: Ref<HTMLInputElement>) => {
+		const value = _value ?? ''
+
 		if (value) {
 			assertDatetimeString(value)
 		}
@@ -17,59 +38,90 @@ export const FallbackDateTimeInput = memo(
 			assertDatetimeString(min)
 		}
 
-		const [maxDate, maxTime] = useMemo(() => splitDatetime(max), [max])
-		const [minDate, minTime] = useMemo(() => splitDatetime(min), [min])
-		const [valueDate, valueTime] = useMemo(() => splitDatetime(value), [value])
-
-		const date = useRef<string>(valueDate)
-		const time = useRef<string>(valueTime)
+		const [state, setState] = useState(value)
+		const [date, time] = splitDatetime(state)
 
 		useEffect(() => {
-			date.current = valueDate
-			time.current = valueTime
-		}, [valueDate, valueTime])
+			setState(value)
+		}, [value])
 
-		const maybeCallOnChange = useCallback(() => {
-			const newValue = date.current && time.current ? `${date.current}T${time.current}` : ''
-
-			if (newValue !== value) {
-				onChange?.(newValue)
-			}
-		}, [onChange, value])
+		useEffect(() => {
+			onChange?.(state)
+		}, [state, onChange])
 
 		const onDateChange = useCallback((event: ChangeEvent<HTMLInputElement>) => {
-			date.current = event.target.value
-			maybeCallOnChange()
-		}, [maybeCallOnChange])
+			setState(joinDatetime(event.target.value, time))
+		}, [time])
 
 		const onTimeChange = useCallback((event: ChangeEvent<HTMLInputElement>) => {
-			time.current = event.target.value
-			maybeCallOnChange()
-		}, [maybeCallOnChange])
+			setState(joinDatetime(date, event.target.value))
+		}, [date])
 
-		return <>
-			<Stack direction="horizontal">
-				<input
-					ref={ref}
-					max={maxDate}
-					min={minDate}
-					onChange={onDateChange}
-					value={valueDate}
-					{...rest}
-					placeholder={rest.placeholder ?? undefined}
-					type="date"
-				/>
-				<input
-					max={date.current && date.current === maxDate ? maxTime : ''}
-					min={date.current && date.current === minDate ? minTime : ''}
-					onChange={onTimeChange}
-					value={valueTime}
-					{...rest}
-					placeholder={undefined}
-					type="time"
-				/>
-			</Stack>
-    </>
+		const dateError = useRef<string | undefined>(undefined)
+		const timeError = useRef<string | undefined>(undefined)
+
+		const changeValidationState = useCallback(() => {
+			onValidationStateChange?.([dateError.current, timeError.current].filter(Boolean).join(' '))
+		}, [onValidationStateChange])
+
+		const dateInputProps = useNativeInput<HTMLInputElement>({
+			...rest,
+			distinction: 'seamless',
+			className: classNames(
+				useComponentClassName('input'),
+				className,
+			),
+			onValidationStateChange: useCallback((error: string | undefined) => {
+				dateError.current = error
+				changeValidationState()
+			}, [changeValidationState]),
+			value: date,
+		}, ref)
+
+		const timeInputRef = useRef<HTMLInputElement>(null)
+		const timeInputProps = useNativeInput<HTMLInputElement>({
+			...rest,
+			distinction: 'seamless',
+			className: classNames(
+				useComponentClassName('input'),
+				className,
+			),
+			onValidationStateChange: useCallback((error: string | undefined) => {
+				timeError.current = error
+				changeValidationState()
+			}, [changeValidationState]),
+			value: time,
+		}, timeInputRef)
+
+		const [maxDate, maxTime] = splitDatetime(max)
+		const [minDate, minTime] = splitDatetime(min)
+
+		return <Stack gap="large" direction="horizontal" className={useInputClassName<VisuallyDependententControlProps>({
+			...rest,
+			className: classNames(
+				useComponentClassName('input'),
+				toViewClass('withTopToolbar', withTopToolbar),
+				className,
+			),
+		})}>
+			<input
+				{...dateInputProps}
+				max={maxDate}
+				min={minDate}
+				onChange={onDateChange}
+				placeholder={rest.placeholder ?? undefined}
+				type="date"
+			/>
+			<Divider gap="none" />
+			<input
+				{...timeInputProps}
+				max={date && date === maxDate ? maxTime : ''}
+				min={date && date === minDate ? minTime : ''}
+				onChange={onTimeChange}
+				placeholder={undefined}
+				type="time"
+			/>
+		</Stack>
 	}),
 )
 FallbackDateTimeInput.displayName = 'FallbackDateTimeInput'
