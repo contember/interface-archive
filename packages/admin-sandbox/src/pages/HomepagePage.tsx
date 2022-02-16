2a60e5753a547815b46b104d76d15890b4ffc81c
Vojta Staněk <stanekv01@gmail.com>
2022-02-16 16:55:31 +0100

    Sandbox: Add buttons to programatically add content

--- packages/admin-sandbox/src/pages/HomepagePage.tsx
+++ packages/admin-sandbox/src/pages/HomepagePage.tsx
@@ -1,8 +1,10 @@
 import { EditPage } from '@contember/admin'
+import { AddContent } from '../components/AddContent'
 import { ContentField } from '../components/ContentField'
 
 export const HomepagePage = (
 	<EditPage pageName="homepage" entity="Homepage(unique = One)">
 		<ContentField field={'content'}/>
+		<AddContent field="content" />
 	</EditPage>
 )
