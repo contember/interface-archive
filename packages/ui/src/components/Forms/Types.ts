8550396ff7feb2231dbbd7b6d32f31df9a357bf6
Martin Adamko <attitude@users.noreply.github.com>
2022-02-23 11:03:38 +0100

    Types

--- packages/ui/src/components/Forms/Types.ts
+++ packages/ui/src/components/Forms/Types.ts
@@ -41,30 +41,17 @@ export interface ControlDisplayProps {
 export interface ControlValueProps<V> {
 	defaultValue?: V | undefined
 	onChange?: (value?: V | null) => void
+	notNull?: boolean
 	placeholder?: string | null
 	value?: V | null
 }
 
-interface RequiredControlValueProps<V> {
-	required: true
-	onChange?: (value: V) => void
-}
-
-interface OptionalControlValueProps<V> {
-	required?: false
-	onChange?: (value?: V | null) => void
-}
-
-interface _OwnControlProps<V> extends
-	ControlValueProps<V>,
-	ValidationSteteProps,
-	ControlStateProps,
-	ControlFocusProps,
-	ControlDisplayProps {}
-
 export type OwnControlProps<V> =
-	| _OwnControlProps<V> & RequiredControlValueProps<V>
-	| _OwnControlProps<V> & OptionalControlValueProps<V>
+	ControlDisplayProps &
+	ValidationSteteProps &
+	ControlStateProps &
+	ControlFocusProps &
+	ControlValueProps<V>
 
 export type AllOwnControlProps<V> = All<OwnControlProps<V>>
 export type OwnControlPropsKeys<V> = keyof OwnControlProps<V>
