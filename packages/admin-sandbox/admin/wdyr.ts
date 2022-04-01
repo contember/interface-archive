44d87cb1b58b6ad2b4232e527159fa827c9d2779
Martin Adamko <attitude@users.noreply.github.com>
2022-04-01 13:04:00 +0200

    Update WDYR

--- packages/admin-sandbox/admin/wdyr.ts
+++ packages/admin-sandbox/admin/wdyr.ts
@@ -1,6 +1,7 @@
 import whyDidYouRender from '@welldone-software/why-did-you-render'
 import React from 'react'
 
+// TODI: Incomplete
 whyDidYouRender(React, {
-  trackAllPureComponents: true,
+  logOwnerReasons: true,
 })
