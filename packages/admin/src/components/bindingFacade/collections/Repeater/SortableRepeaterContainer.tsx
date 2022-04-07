423a15799ad82d2042fc565c583925dcccfc8bfb
Martin Adamko <attitude@users.noreply.github.com>
2022-04-07 10:26:25 +0200

    Resolve root element for sortable container

--- packages/admin/src/components/bindingFacade/collections/Repeater/SortableRepeaterContainer.tsx
+++ packages/admin/src/components/bindingFacade/collections/Repeater/SortableRepeaterContainer.tsx
@@ -1,11 +1,20 @@
-import { memo, ReactNode } from 'react'
-import { SortableContainer } from 'react-sortable-hoc'
-
-export interface SortableRepeaterContainerProps {
+import { ReactNode } from 'react'
+import { HelperContainerGetter, SortableContainer, SortableContainerProps } from 'react-sortable-hoc'
+export interface SortableRepeaterContainerProps extends SortableContainerProps {
 	children: ReactNode
 }
 
-export const SortableRepeaterContainer = memo(
-	SortableContainer(({ children }: SortableRepeaterContainerProps) => <>{children}</>),
-)
+const getHelperContainer: HelperContainerGetter = () => {
+	console.log(document.getElementById('portal-root'))
+
+	return document.getElementById('portal-root') ?? document.body
+}
+
+const SortableRepeaterContainerInner = SortableContainer(({ children }: SortableRepeaterContainerProps) => <>{children}</>)
+
+export const SortableRepeaterContainer = (props: SortableRepeaterContainerProps) => <SortableRepeaterContainerInner
+	helperContainer={getHelperContainer}
+	{...props}
+/>
+
 SortableRepeaterContainer.displayName = 'SortableRepeaterContainer'
