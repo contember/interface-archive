a57c4a3c2381a83910343bbbfbecdb69c690e2ea
Martin Adamko <attitude@users.noreply.github.com>
2022-07-07 14:13:04 +0200

    Update RepeaterItemContainer: Use new Box
    - Disable user select on labels & handle
    - Support horizontal layout

--- packages/ui/src/components/RepeaterItemContainer/RepeaterItemContainer.tsx
+++ packages/ui/src/components/RepeaterItemContainer/RepeaterItemContainer.tsx
@@ -1,30 +1,46 @@
 import classNames from 'classnames'
 import { ComponentType, memo, ReactNode } from 'react'
 import { useClassNamePrefix } from '../../auxiliary'
-import { Size } from '../../types'
 import { toViewClass } from '../../utils'
-import { Box } from '../Box'
+import { Box, BoxOwnProps } from '../Box'
 import { Icon } from '../Icon'
-import { Stack } from '../Stack'
+import { Stack, StackOwnProps } from '../Stack'
 import { Label } from '../Typography/Label'
 
-export interface RepeaterItemContainerProps {
-	gap?: Size
-	label?: ReactNode
+export interface RepeaterItemContainerOwnProps {
 	actions?: ReactNode
 	children: ReactNode
+	direction?: StackOwnProps['direction']
 	dragHandleComponent?: ComponentType<{ children: ReactNode }>
+	label?: ReactNode
+	scrollable?: boolean
 }
 
-export const RepeaterItemContainer = memo(({ actions, children, gap, label, dragHandleComponent: Handle }: RepeaterItemContainerProps) => {
+export type RepeaterItemContainerProps =
+	& RepeaterItemContainerOwnProps
+	& BoxOwnProps
+
+
+export const RepeaterItemContainer = memo(({
+	actions,
+	children,
+	className,
+	gap = true,
+	direction,
+	dragHandleComponent: Handle,
+	label,
+	...props
+}: RepeaterItemContainerProps) => {
 	const componentClassName = `${useClassNamePrefix()}repeater-item-container`
 
 	return (
 		<Box
+			{...props}
 			gap={gap}
 			className={classNames(
 				componentClassName,
 				toViewClass('sortable', !!Handle),
+				className,
 			)}
 		>
 			{Handle && (
@@ -45,8 +61,9 @@ export const RepeaterItemContainer = memo(({ actions, children, gap, label, drag
 				{actions && <div className={`${componentClassName}-actions`}>{actions}</div>}
 			</div>}
 			<Stack
+				span
 				className={`${componentClassName}-content`}
-				direction="vertical"
+				direction={direction}
 				gap={gap}
 			>
 				{children}
