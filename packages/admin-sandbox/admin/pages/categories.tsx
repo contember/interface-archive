2f02f60a9e96d93a7d0f97bc207f00fd362f3f9a
Martin Adamko <attitude@users.noreply.github.com>
2022-07-07 18:55:57 +0200

    Admin Sandbox: Update pages

--- packages/admin-sandbox/admin/pages/categories.tsx
+++ packages/admin-sandbox/admin/pages/categories.tsx
@@ -1,15 +1,55 @@
-import { Component, MultiEditPage, Repeater, SelectField, TextField } from '@contember/admin'
+import { Component, MultiEditPage, Repeater, SideDimensions, TextField, View } from '@contember/admin'
 
 export const CategoryForm = Component(() => <>
-	<TextField field={'name'} label={'Name'} />
-	<Repeater field={'locales'} label={'Locales'} orderBy={'id'}>
-		<SelectField label={'Locale'} options={'Locale.code'} field={'locale'} createNewForm={<TextField field={'code'} label={'Locale code'} />} />
+	<View span={1}>
 		<TextField field={'name'} label={'Name'} />
-	</Repeater>
+	</View>
+	<View span={2}>
+		<Repeater
+			compact
+			scrollable
+			direction="horizontal"
+			field={'locales'}
+			label={'Locales'}
+			orderBy={'id'}
+		>
+				{/* <View span={2}>
+					<SelectField label={'Locale'} options={'Locale.code'} field={'locale'} createNewForm={<TextField field={'code'} label={'Locale code'} />} />
+				</View>
+				<View span={1}>
+					<TextField width="fluid" field={'name'} label={'Name'} />
+				</View> */}
+
+				<SideDimensions
+					dimension="locale"
+					variableName="currentLocaleCode"
+					variables={currentLocaleCode => {
+						return {
+							labelMiddleware: label => (
+								<>
+									{label} ({currentLocaleCode})
+								</>
+							),
+						}
+					}}
+				>
+					<TextField width="fluid" field={'name'} label={'Name'} />
+				</SideDimensions>
+		</Repeater>
+	</View>
 </>)
 
+
 export default () => (
-	<MultiEditPage entities="Category" rendererProps={{ title: 'Categories', sortableBy: 'order' }}>
+	<MultiEditPage
+		entities="Category"
+		rendererProps={{
+			fit: 'none',
+			pageContentLayout: 'start',
+			sortableBy: 'order',
+			title: 'Categories',
+		}}
+	>
 		<CategoryForm/>
 	</MultiEditPage>
 )
--- packages/admin-sandbox/admin/pages/tags.tsx
+++ packages/admin-sandbox/admin/pages/tags.tsx
@@ -1,12 +1,38 @@
-import { MultiEditPage, Repeater, SelectField, TextField } from '@contember/admin'
+import { Icon, LinkButton, MultiEditPage, Repeater, SelectField, TextField, View } from '@contember/admin'
 
 export default () => (
-	<MultiEditPage entities="Tag" rendererProps={{ title: 'Abcd' }}>
-		<TextField field={'name'} label={'Name'} />
-		<Repeater field={'locales'} label={'Locales'} sortableBy={'order'}>
-			<SelectField label={'Locale'} options={'Locale.code'} field={'locale'}
-									 createNewForm={<TextField field={'code'} label={'Locale code'} />} />
+	<MultiEditPage
+		compact
+		scrollable
+		entities="Tag"
+		rendererProps={{
+			title: 'Tags',
+			pageContentLayout: 'start',
+			containerComponentExtraProps: {
+				maxHeight: '--cui-page-content-height',
+			},
+			itemComponentExtraProps: {
+				actions: <LinkButton flow="circular" size="small" distinction="seamless" to="tagEdit(id: $entity.id)"><Icon blueprintIcon="edit" /></LinkButton>,
+			},
+		}}
+	>
+		<View span={1}>
 			<TextField field={'name'} label={'Name'} />
-		</Repeater>
+		</View>
+		<View span={2}>
+			<Repeater compact field={'locales'} label={'Locales'} sortableBy={'order'}>
+				<SelectField
+					labelPosition="labelLeft"
+					label={'Locale'}
+					options={'Locale.code'}
+					field={'locale'}
+					createNewForm={<TextField field={'code'} label={'Locale code'} />}
+				/>
+				<TextField
+					labelPosition="labelLeft"
+					field={'name'} label={'Name'}
+				/>
+			</Repeater>
+		</View>
 	</MultiEditPage>
 )
