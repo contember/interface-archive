072be8ec96dcb7ea35d83f8e15406f26275d687c
Martin Adamko <attitude@users.noreply.github.com>
2022-07-07 14:17:55 +0200

    Update RepeaterItemContainer Playwright tests & styles

--- packages/admin/tests/playwright/cases/repeaterItemContainer.tsx
+++ packages/admin/tests/playwright/cases/repeaterItemContainer.tsx
@@ -1,26 +1,59 @@
-import { Layout, LayoutPage, RepeaterItemContainer, StyleProvider } from '@contember/ui'
+import { Button, FieldContainer, Layout, LayoutPage, RepeaterItemContainer, Stack, StyleProvider } from '@contember/ui'
 
 const dummyInput = (
-	<div style={{ backgroundColor: 'lightBlue', border: '1px solid blue', borderRadius: '0.25em', height: '2.25em', minWidth: '2.25em' }}></div>
+	<div style={{ backgroundColor: 'lightBlue', border: '1px solid blue', borderRadius: '0.25em', height: '2.25em', minWidth: '10em' }}></div>
 )
 
 const Handle = () => <>=</>
+
+const addButton = <Button distinction="seamless" justification="justifyStart">+ Add new item</Button>
 
 export default function () {
 	return (
 		<StyleProvider>
 			<Layout>
-				<LayoutPage>
-					<RepeaterItemContainer dragHandleComponent={Handle}>
-						{dummyInput}
-						{dummyInput}
-						{dummyInput}
-					</RepeaterItemContainer>
-					<RepeaterItemContainer>
-						{dummyInput}
-						{dummyInput}
-						{dummyInput}
-					</RepeaterItemContainer>
+				<LayoutPage title="Repeater Item Container">
+					<Stack direction="horizontal" align="flex-start" wrap>
+						<RepeaterItemContainer dragHandleComponent={Handle}>
+							{dummyInput}
+							{dummyInput}
+							{dummyInput}
+						</RepeaterItemContainer>
+						<RepeaterItemContainer>
+							{dummyInput}
+							{dummyInput}
+							{dummyInput}
+						</RepeaterItemContainer>
+						<RepeaterItemContainer dragHandleComponent={Handle} label="Lorem ipsum">
+							{dummyInput}
+							{dummyInput}
+							{dummyInput}
+						</RepeaterItemContainer>
+					</Stack>
+
+					<FieldContainer useLabelElement={false} footer={addButton} label="Horizontal Repeater" gap="border" direction="horizontal" wrap>
+						<RepeaterItemContainer compact dragHandleComponent={Handle} label="Lorem ipsum">
+							{dummyInput}
+						</RepeaterItemContainer>
+						<RepeaterItemContainer compact dragHandleComponent={Handle} label="Lorem ipsum">
+							{dummyInput}
+						</RepeaterItemContainer>
+						<RepeaterItemContainer compact dragHandleComponent={Handle} label="Lorem ipsum">
+							{dummyInput}
+						</RepeaterItemContainer>
+					</FieldContainer>
+
+					<FieldContainer useLabelElement={false} footer={addButton} label="Vertical Repeater" gap="border">
+						<RepeaterItemContainer compact dragHandleComponent={Handle} label="Lorem ipsum">
+							{dummyInput}
+						</RepeaterItemContainer>
+						<RepeaterItemContainer compact dragHandleComponent={Handle} label="Lorem ipsum">
+							{dummyInput}
+						</RepeaterItemContainer>
+						<RepeaterItemContainer compact dragHandleComponent={Handle} label="Lorem ipsum">
+							{dummyInput}
+						</RepeaterItemContainer>
+					</FieldContainer>
 				</LayoutPage>
 			</Layout>
 		</StyleProvider>
