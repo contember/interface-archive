d89d496ccdc8eedc21ba5f903102018a44674865
Martin Adamko <attitude@users.noreply.github.com>
2022-08-12 19:01:42 +0200

    Remove placeholders & Add callbacks for resolvers

--- packages/ui/src/components/StyleSheet/Helpers.ts
+++ packages/ui/src/components/StyleSheet/Helpers.ts
@@ -1,4 +1,6 @@
+import { CLASS_NAME_RESET_STRING } from '.'
 import { isNonEmptyString } from './TypePredicates'
+import { StyleSheetValueResolver } from './Types'
 
 type TypedObjectEntries = <K extends PropertyKey, V>(o: { [P in K]?: V }) => [K, V][]
 type TypedObjectFromEntries = <K extends PropertyKey, V>(entries: [K, V][]) => Record<K, V>
@@ -10,12 +12,21 @@ export function excludeFromArray<T, E extends T>(exclude: E, array: Array<T>): A
   return array.filter(value => value !== exclude) as Array<Exclude<T, E>>
 }
 
+export const KEEP_RESET_CLASS_NAME = true
+export const REMOVE_RESET_CLASS_NAME = false
+
+export function filterResetClassNames<T extends any>(value: T[], keepResetClassName: boolean): T[] {
+  const index = value.lastIndexOf(CLASS_NAME_RESET_STRING as T)
+
+  return index < 0 ? value : value.slice(index + (keepResetClassName ? 0 : 1))
+}
+
 export function deepMerge(base: any, extension: any) {
   if (extension === undefined) {
     return base
   } else if (typeof base === typeof extension) {
     if (Array.isArray(base) && Array.isArray(extension)) {
-      return [...base, ...extension]
+      return filterResetClassNames([...base, ...extension], KEEP_RESET_CLASS_NAME)
     } else if (base && typeof base === 'object' && !Array.isArray(base) && !Array.isArray(extension)) {
       const keys = [...new Set([...Object.keys(base), ...Object.keys(extension)])]
       const entries: [string, any][] = keys.map(
@@ -37,6 +48,8 @@ export function splitStringToStringList(value: string): string[] {
 
 const REASONABLE_ARRAY_FLAT_DEPTH = 9
 
-export function toFlatArrayOfNonEmptyStrings(value: any[]): string[] {
-  return value.flat(REASONABLE_ARRAY_FLAT_DEPTH).filter(isNonEmptyString).map(splitStringToStringList).flat(1)
+export function toFlatArrayOfClassNameValues(value: any[]): (string | StyleSheetValueResolver)[] {
+  return excludeFromArray(null, value.flat(REASONABLE_ARRAY_FLAT_DEPTH).map(
+    value => isNonEmptyString(value) ? splitStringToStringList(value) : typeof value === 'function' ? value : null,
+  )).flat(1)
 }
