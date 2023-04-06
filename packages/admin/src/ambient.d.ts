0fb4c54344a47d98402cee36641a4d5ce9ae02a9
Martin Adamko <attitude@users.noreply.github.com>
2023-04-06 11:03:20 +0200

    Remove all ambient.d.ts files

--- packages/admin/src/ambient.d.ts
+++
@@ -1,9 +0,0 @@
-/// <reference types="vite/client" />
-
-interface Document {
-	caretPositionFromPoint?: (x: number, y: number) => {
-		offsetNode: Node
-		offset: number
-		getClientRect(): DOMRect
-	}
-}
