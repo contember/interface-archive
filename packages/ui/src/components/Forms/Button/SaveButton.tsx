ae79a807be24d9d02d82936a0f36250b20e157fb
Martin Adamko <attitude@users.noreply.github.com>
2023-08-01 16:20:53 +0200

    refactor(ui): Simplify Menu, Buttons & Icons
    - Rename CSS custom properties

--- packages/ui/src/components/Forms/Button/SaveButton.tsx
+++ packages/ui/src/components/Forms/Button/SaveButton.tsx
@@ -1,5 +1,5 @@
 import { forwardRef, memo } from 'react'
-import { Button, ButtonProps } from './Forms/Button'
+import { Button, ButtonProps } from '.'
 
 export type SaveButtonProps =
 	& {
@@ -28,10 +28,12 @@ export const SaveButton = memo(forwardRef<HTMLButtonElement, SaveButtonProps>(({
 }, ref) => (
 	<Button
 		ref={ref}
-		size={size ?? 'large'}
+		componentClassName={['button', 'save-button']}
+		className={[rest.className]}
+		distinction={isPrimary ? 'primary' : distinction}
 		flow={flow}
 		scheme={scheme}
-		distinction={isPrimary ? 'primary' : distinction}
+		size={size}
 		{...rest}
 	>
 		{isDirty
