89a9e1f0834f95aa05c819d348febc34200cda34
Martin Adamko <attitude@users.noreply.github.com>
2023-09-15 13:06:02 +0200

    fix(ui): Auto-size `FileDropZone`
    - Fixes overflowing on small screens & in small containers

--- packages/ui/src/components/Forms/Upload/FileDropZone.tsx
+++ packages/ui/src/components/Forms/Upload/FileDropZone.tsx
@@ -1,8 +1,10 @@
-import { useClassNameFactory } from '@contember/react-utils'
-import { forwardRef, memo, ReactNode } from 'react'
+import { useClassNameFactory, useComposeRef } from '@contember/react-utils'
+import { ReactNode, forwardRef, memo, useLayoutEffect, useState } from 'react'
 import { HTMLDivElementProps } from '../../../types'
 import { toStateClass } from '../../../utils'
 import { Box } from '../../Box'
+
+const REASONABLE_COLUMN_COUNT = 24
 
 export type FileDropZoneProps =
 	& {
@@ -25,16 +27,56 @@ export const FileDropZone = memo(forwardRef<HTMLDivElement, FileDropZoneProps>((
 }, ref) => {
 	const componentClassName = useClassNameFactory('fileDropZone')
 
+	const [boxElement, setBoxElement] = useState<HTMLDivElement | null>(null)
+	const composeRef = useComposeRef(ref, setBoxElement)
+
+	useLayoutEffect(() => {
+		// We need element ready & respect style overrides:
+		if (boxElement && !boxElement.style.gridColumnStart && !boxElement.style.gridColumnEnd) {
+			// Set the cell to its initial position, get offset:
+			boxElement.style.gridColumnStart = 'auto'
+			boxElement.style.gridColumnEnd = 'auto'
+			const offsetLeft = boxElement.offsetLeft
+
+			let gridColumnStart: number | null = null
+
+			// Iterate until we find the cell's start column:
+			for (let i = 1; i < REASONABLE_COLUMN_COUNT; i++) {
+				boxElement.style.gridColumnStart = `${i}`
+
+				const currentOffsetLeft = boxElement.offsetLeft
+
+				if (currentOffsetLeft <= offsetLeft) {
+					gridColumnStart = i
+				} else {
+					break
+				}
+			}
+
+			if (gridColumnStart !== null) {
+				boxElement.style.setProperty('--fileDropZone--grid-column-start', `${gridColumnStart}`)
+				boxElement.style.setProperty('--fileDropZone--grid-column-end', '-1')
+			} else {
+				boxElement.style.setProperty('--fileDropZone--grid-column-start', '')
+				boxElement.style.setProperty('--fileDropZone--grid-column-end', '')
+			}
+
+			// Reset the cell's position:
+			boxElement.style.gridColumnStart = ''
+			boxElement.style.gridColumnEnd = ''
+		}
+	})
+
 	return (
 		<Box
 			{...props}
+			ref={composeRef}
 			className={componentClassName(null, [
 				toStateClass('active', isActive),
 				toStateClass('accepting', isAccepting),
 				toStateClass('rejecting', isRejecting),
 				className,
 			])}
-			ref={ref}
 		/>
 	)
 }))
